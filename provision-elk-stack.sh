ac52d8d971aedfa1f4757497b3ac94a012808fcd
Aaron Lindstrom <Aaron.Lindstrom@savogroup.com>
2015-08-12 11:35:31 -0500

    Added echos for sections of the script that are running

--- provision-elk-stack.sh
+++ provision-elk-stack.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get -y install oracle-java8-installer
 
 
-## Install Elasticsearch #######################################################
+echo "### Install Elasticsearch #######################################################"
 wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
 echo 'deb http://packages.elasticsearch.org/elasticsearch/1.7/debian stable main' | sudo tee /etc/apt/sources.list.d/elasticsearch.list
 sudo apt-get update
@@ -25,12 +25,12 @@ sudo update-rc.d elasticsearch defaults 95 10
 #### install kopf
 sudo /usr/share/elasticsearch/bin/plugin -install lmenezes/elasticsearch-kopf
 
-## Install Logstash ############################################################
+echo "### Install Logstash ############################################################"
 sudo apt-get update
 sudo apt-get -y install logstash
 sudo cp /vagrant-data/logstash.conf /etc/logstash/conf.d/logstash.conf
 
-## Install Kibana ##############################################################
+echo "### Install Kibana ##############################################################"
 cd ~; wget https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz
 tar xvf kibana-*.tar.gz
 sudo cp /vagrant-data/kibana.yml ~/kibana-4*/config/kibana.yml
