364fede37712961e275c3f63cfec5176f6e5a28d
Aaron Lindstrom <Aaron.Lindstrom@savogroup.com>
2015-08-12 14:19:58 -0500

    Added lines to auto-accept Oracle license on install

--- provision-elk-stack.sh
+++ provision-elk-stack.sh
@@ -5,6 +5,8 @@
 echo "### Install Oracle Java 8 ###############################################"
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get -y install oracle-java8-installer
 
 
