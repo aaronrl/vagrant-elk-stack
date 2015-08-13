894564112466fce90fca779b39452d4fff1fc21b
Aaron Lindstrom <Aaron.Lindstrom@savogroup.com>
2015-08-13 09:10:04 -0500

    Reduced verbosity of some commands so the box initialization isn't so crazy to read through.

--- provision-elk-stack.sh
+++ provision-elk-stack.sh
@@ -37,13 +37,13 @@ sudo rm -f /etc/logstash/conf.d/logstash.conf
 sudo ln -s /vagrant-data/logstash.conf /etc/logstash/conf.d/logstash.conf
 
 echo "### Download Kibana #####################################################"
-cd ~; wget https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz
-tar xvf kibana-*.tar.gz
+cd ~; wget -nv https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz
+tar xf kibana-*.tar.gz
 
 echo "### Install Kibana ######################################################"
 sudo mkdir -p /opt/kibana
 sudo cp -R ~/kibana-4*/* /opt/kibana/
-cd /etc/init.d && sudo wget https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4
+cd /etc/init.d && sudo wget -nv https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4
 sudo chmod +x /etc/init.d/kibana4
 sudo update-rc.d kibana4 defaults 96 9
 
